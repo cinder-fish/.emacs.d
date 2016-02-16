5fd437f6bdf221ff1904940b8d79d79f435d0c65
Alex Brown <al.p.brown@gmail.com>
2016-02-16 19:18:00 +0000

    Unify standard clojure settings

--- settings/clojure-settings.el
+++ settings/clojure-settings.el
@@ -1,14 +1,19 @@
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(defun clojure-settings/add-hooks (hooks fns)
+  (cl-loop for hook in hooks
+           for fn in fns
+           do (add-hook hook fn)))
+
+(clojure-settings/add-hooks
+ '(clojure-mode-hook
+   clojurescript-mode-hook
+   cider-mode-hook)
+ '(paredit-mode
+   rainbow-delimiters-mode))
 
 (setq nrepl-hide-special-buffers t)
 (setq cider-repl-pop-to-buffer-on-connect nil)
 (setq cider-show-error-buffer nil)
 
-(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
 (define-key clojure-mode-map (kbd "C-c r") 'cljr-helm)
