483030f2376e57fbf9fecb510dae98c6af71fedb
Alex Brown <al.p.brown@gmail.com>
2016-07-07 10:18:20 +0100

    Fix not working clojure hooks binding

--- settings/clojure-settings.el
+++ settings/clojure-settings.el
@@ -1,12 +1,13 @@
+(require 'dash)
 (defun clojure-settings/add-hooks (hooks fns)
-  (cl-loop for hook in hooks
-           for fn in fns
-           do (add-hook hook fn)))
+  (--each (-table-flat 'cons hooks fns)
+    (add-hook (car it) (cdr it))))
 
 (clojure-settings/add-hooks
  '(clojure-mode-hook
    clojurescript-mode-hook
-   cider-mode-hook)
+   cider-mode-hook
+   cider-repl-mode-hook)
  '(paredit-mode
    rainbow-delimiters-mode
    eldoc-mode))
@@ -14,5 +15,8 @@
 (setq nrepl-hide-special-buffers t)
 (setq cider-repl-pop-to-buffer-on-connect nil)
 (setq cider-show-error-buffer nil)
+(setq cider-repl-use-pretty-printing t)
+
+(setq cljr-warn-on-eval nil)
 
 (define-key clojure-mode-map (kbd "C-c r") 'cljr-helm)
