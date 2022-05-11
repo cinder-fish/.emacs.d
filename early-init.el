7230870939d06f7b4fcb14d0f2968b994b7a4d0f
Alex Brown <al.p.brown@gmail.com>
2022-05-11 20:14:09 +0100

    Tweak early startup

--- early-init.el
+++ early-init.el
@@ -1,1 +1,16 @@
-(setq comp-speed 2)
+(setq
+ ;; Disable autoloads
+ site-run-file nil
+ inhibit-default-init t
+
+ ;; No GC during init
+ gc-cons-threshold most-positive-fixnum
+
+ ;; Ensure native-comp optimisations
+ comp-speed 2
+ native-comp-speed 2)
+
+;; Reset GC to 20M
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 20 1024 1024))))
